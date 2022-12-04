a84961b7206213568de46690177d3c1db0634249
Tyler <tylermauch18@gmail.com>
2022-12-03 23:46:19 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './mainBanner.css';
 import reportWebVitals from './reportWebVitals';
+
+
+
+// Pomo-time? Pomo Clock? Pomo Track?
+// Doro-time?
+class Main extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+
+    }
+  }
+  demoButton(){
+    return(
+    <div id="demoButton">Go to Dash</div>
+    )
+
+  }
+  topBanner(){
+    return(
+    <div id="topBanner">
+      <div id="topBannerNavBar">
+        <ul id="topBannerNavBar">
+          <li className="topBannerNavItem"><a href="#">Home</a></li>
+          <li className="topBannerNavItem"><a href="#">About</a></li>
+          <li className="topBannerNavItem"><a href="#">Timer</a></li>
+        </ul>
+      </div>
+      <img id="topBannerLogo" src="dorotimeLogo.png"/>
+      <hr id="topBannerBreak"/>
+    </div>
+    )
+  }
+  mainBanner(){
+    return(
+      <div id="mainBanner">
+        <img src="lowPolyDoro.png" id="polyBanner"/>
+        <div id="mainBannerHeader">Improve your Focus.</div>
+        <div id="mainBannerParagraph">Stay on task longer, and tackle the toughest problems with Dorotime!</div>
+      </div>
+
+    )
+
+  }
+  render(){
+    return(
+      <>
+        <>{this.topBanner()}</>
+        <>{this.mainBanner()}</>
+        <>{this.demoButton()}</>
+      </>
+    )
+    }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div id="container">
+    <Main/>
+  </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/timer/timer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from './react-dom/client';
+import './timer.css'
+
+class Timer extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state={
+
+
+        }
+
+    }
+
+}
