c21155e1dbb9652ab118ece7e4c3e82ad36d639e
Tyler <tylermauch18@gmail.com>
2022-12-19 16:56:07 -0500

    basic cookie counter added to timer

--- src/Components/timerComponent.js
+++ src/Components/timerComponent.js
@@ -1,7 +1,30 @@
 import React, {useState} from 'react'
 import BlueButton from './blueButton';
 import './ComponentStyles/timerComponent.css'
-const TimerComponent = (props) => { //FIXME: Minutes displayed jumps when seconds reaches 0
+const TimerComponent = (props) => { //FIXME (or not): Minutes displayed jumps when seconds reaches 0
+    const DistractCookieGetter = ()=>{ // timer.js will handle total distractions after this is complete
+        if(document.cookie.indexOf("session_distraction_count=")===-1){
+            return -1
+        }
+        const decodeCookie = decodeURIComponent(document.cookie)
+        const cookieArray = decodeCookie.split(";")
+        const cookieArrayIndexed=cookieArray[document.cookie.indexOf("session_distraction_count")]
+        const sesDistractCountValue=cookieArrayIndexed.split("=")[1]
+        return sesDistractCountValue
+    }
+    const DistractCookieSetter = () => {
+        if(DistractCookieGetter()===-1){
+            console.log("Cookie does not exist, creating cookie")
+            document.cookie="session_distraction_count=1";
+        }
+        else{
+            const sesDistractCountValue=DistractCookieGetter()
+            console.log("Cookie already exists, altering")
+            console.log(sesDistractCountValue)
+            document.cookie=("session_distraction_count="+(parseInt(sesDistractCountValue)+1).toString());
+            
+        }
+    }
     var displayMinutes=props.minutesLeft.toString()
     var displaySeconds=props.secondsLeft.toString()
 
@@ -26,8 +49,8 @@ const TimerComponent = (props) => { //FIXME: Minutes displayed jumps when second
             case(true):
             return (
                 <>
-                <div id="distractionCounter">Distractions: COOKIES</div>
-                <BlueButton purpose="generic" idKey="timerDistraction" content="Distracted"/>
+                <div id="distractionCounter">Distractions: {DistractCookieGetter()===-1 ? "0" : DistractCookieGetter()}</div>
+                <div onClick={()=>DistractCookieSetter()}><BlueButton purpose="generic" idKey="timerDistraction" content="Distracted"/></div>
                 <div id={props.timerSection=="Work Time" ? "timerDisplayHeaderWork":"timerDisplayHeaderBreak"}>{props.timerSection}</div>
                 <div id={props.timerSection=="Work Time" ? "timerDisplayWork":"timerDisplayBreak"}>{displayMinutes}:{displaySeconds}</div>
                 </>
