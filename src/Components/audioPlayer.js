9f87dc4d75fbc003ed45e2e1fe1786095002c518
Tyler <tylermauch18@gmail.com>
2022-12-28 19:09:24 -0500

    audio bug fix

--- src/Components/audioPlayer.js
+++ src/Components/audioPlayer.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 const AudioPlayer = (props) => {
     const track = props.track
-    return(
-        <audio src ={track.source} autoPlay loop/>
-    )
+    switch(track){
+        case(null):
+            return(null)
+            break
+        default:
+            return(
+                <audio src ={track.source} autoPlay loop/>
+            )
+    }
+
 
     }
 export default AudioPlayer
